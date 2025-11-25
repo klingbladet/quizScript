245899cc4a5f8b1239fd45f295dffdd82b78652a
Housame Oueslati <housame.oueslati@chasacademy.se>
2025-11-25 15:12:48 +0100

    totally 6 modules

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ import { showstart, countdownDisplay, startButton, leaderboardButton, pauseBtn, 
 import { startCountdown, updateCountdown, TOTAL_TIME_SECONDS, countdownTime, state, togglePause, countdownInterval } from "./scripts/timer.js";
 import { draw } from "./scripts/matrix.js";
 import { getQuizQuestions } from "./scripts/questions.js";
+import { showLeaderboard } from "./scripts/leaderBoard.js";
 
 console.log("Script loaded successfully.");
 
@@ -73,10 +74,6 @@ function resultatRestartGame() {
   init();
 }
 
-
-
-
-
 function waitForAnswer(answerElements) {
   return new Promise((resolve) => {
     answerElements.forEach((btn, index) => {
@@ -90,9 +87,6 @@ function waitForAnswer(answerElements) {
     });
   });
 }
-
-
-
 
 function waitForUnpause() {
   if (!state.isPaused) return Promise.resolve();
@@ -251,53 +245,7 @@ function restartQuiz() {
 
 
 
-async function showLeaderboard() {
-  const container = document.getElementById("leaderboard-container");
-  const list = document.getElementById("leaderboard-list");
-  list.innerHTML = "";
 
-  const results = await loadResults();
-
-  if (!results || results.length === 0) {
-    list.innerHTML = `<li>Inga resultat hittades ännu.</li>`;
-    container.classList.remove("hidden");
-    return;
-  }
-
-  results.sort((a, b) => {
-    if (b.score !== a.score) return b.score - a.score;
-    return b.timeRemaining - a.timeRemaining;
-  });
-
-  const topTen = results.slice(0, 10);
-
-  topTen.forEach((r, i) => {
-    const li = document.createElement("li");
-
-    if (i === 0) li.classList.add("firstRanked");
-    else if (i === 1) li.classList.add("secondRanked");
-    else if (i === 2) li.classList.add("thirdRanked");
-    else li.classList.add("ranked");
-
-    let rankDisplay;
-    if (i === 0) rankDisplay = "#1";
-    else if (i === 1) rankDisplay = "#2";
-    else if (i === 2) rankDisplay = "#3";
-    else rankDisplay = `#${i + 1}`;
-
-    li.innerHTML = `
-      <span class="rank">${rankDisplay}</span>
-      <span class="user">${r.userName || "Anonym"}</span>
-      <span class="score">${r.score}/${r.total} – ${r.timeUsed}</span>
-    `;
-
-    list.appendChild(li);
-  });
-
-  container.classList.remove("hidden");
-  document.getElementById("question-container").classList.add("hidden");
-  document.getElementById("result-container").classList.add("hidden");
-}
 
 
 
